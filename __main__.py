b307ef6683942bf6a33d4a017fbc4aa5ab36a668
hesham moneer <hesham.moneer@outlook.com>
2022-06-25 03:17:00 +0200

    main class updates

--- __main__.py
+++ __main__.py
@@ -1,19 +1,26 @@
+from align_faces import align, fom
 import cv2
 
-from dlib_implementation import detectLmsDlib, filterMouthLmsDlib
+from dlib_implementation import detectLmsDlib, faceSquareDlib, filterMouthLmsDlib
 from draw_lms import drawCircles, drawDelaunayMesh, drawMeshMP
-from mediapipe_implementation import detectLmsMP, filterMouthLmsMP
-
+from mediapipe_implementation import detectLmsMP, faceRegionMP, filterFaceBoundsLmsMP, filterFaceLmsMP, filterMouthLmsMP
+from get_lm import getLM
+from delaunay_triangulation import make_delaunay
 
 def main():
   cap = cv2.VideoCapture(0)
+  img = cv2.imread('samples/img1.jpg')
+  # cv2.namedWindow('image')
   while True:
     ret, frame = cap.read()
     if not ret: break
-    lms = detectLmsMP(frame)
-    lms = filterMouthLmsMP(lms)
-    drawMeshMP(frame, lms)
-    cv2.imshow('', frame)
+    frame = align(img, frame)
+    # lms = detectLmsMP(frame)
+    # cv2.setMouseCallback('image', getLM, lms)
+    # lms = filterFaceLmsMP(lms)
+    # drawDelaunayMesh(frame, lms)
+    # drawCircles(frame, lms)
+    cv2.imshow('image', frame)
     if cv2.waitKey(1) & 0xFF == 27: break
 
 
